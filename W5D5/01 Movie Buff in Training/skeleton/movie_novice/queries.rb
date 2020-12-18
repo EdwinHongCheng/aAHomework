5072adbc2871376b75fcba9dda00104d352d7bc9
Edwin Cheng <edwincheng3@gmail.com>
2020-12-18 02:12:25 -0800

    Added W5D5 HW

---
+++ W5D5/01 Movie Buff in Training/skeleton/movie_novice/queries.rb
@@ -0,0 +1,117 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id          :integer      not null, primary key
+#  name        :string
+#
+# Table name: movies
+#
+#  id          :integer      not null, primary key
+#  title       :string
+#  yr          :integer
+#  score       :float
+#  votes       :integer
+#  director_id :integer
+#
+# Table name: castings
+#  id          :integer      not null, primary key
+#  movie_id    :integer      not null
+#  actor_id    :integer      not null
+#  ord         :integer
+
+
+# Self-Notes 
+
+# to test my query:
+# Rails Console: just write my entire query as one connected line
+# see the below for examples, can try it out in Rails Console 
+# to use Rails Console, run: rails c
+
+def find_angelina # Finished
+  #find Angelina Jolie by name in the actors table
+
+
+  # Rails Console: same thing
+  Actor.find_by(name: 'Angelina Jolie')
+end
+
+def top_titles # Finished
+  # get movie titles from movies with scores greater than or equal to 9
+  # hint: use 'select' and 'where'
+
+
+  # Note: in Rails Console: run - Movie.select(:id, :title).where('score >= 9')
+  Movie
+    .select(:id, :title)
+    .where('score >= 9')
+end
+
+def star_wars # Finished
+  #display the id, title and year of each Star Wars movie in movies.
+  # hint: use 'select' and 'where'
+
+
+  # Rails Console: Movie.select(:id, :title, :yr).where('title LIKE \'%Star Wars%\'')
+  Movie
+    .select(:id, :title, :yr)
+    .where('title LIKE \'%Star Wars%\'')
+end
+
+
+def below_average_years # Finished
+  #display each year with movies scoring under 5,
+  #with the count of movies scoring under 5 aliased as bad_movies,
+  #in descending order
+  # hint: use 'select', 'where', 'group', 'order'
+
+
+  # Rails Console: Movie.select('yr', 'COUNT(*) as bad_movies').where('score < 5').group('yr').order('bad_movies DESC')
+  Movie
+    .select('yr', 'COUNT(*) as bad_movies')
+    .where('score < 5')
+    .group('yr')
+    .order('bad_movies DESC')
+end
+
+def alphabetized_actors # Finished
+  # display the first 10 actor names ordered from A-Z
+  # hint: use 'order' and 'limit'
+  # Note: Ubuntu users may find that special characters
+  # are alphabetized differently than the specs.
+  # This spec might fail for Ubuntu users. It's ok!
+
+
+  # Rails Console: Actor.order('name ASC').limit(10)
+  Actor
+    .order('name ASC')
+    .limit(10)
+end
+
+def pulp_fiction_actors # Finished
+  # practice using joins
+  # display the id and name of all actors in the movie Pulp Fiction
+  # hint: use 'select', 'joins', 'where'
+
+
+  # Rails Console: Actor.select(:id, :name).joins(:movies).where('title = \'Pulp Fiction\'')
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where('title = \'Pulp Fiction\'')
+end
+
+def uma_movies # Finished
+  #practice using joins
+  # display the id, title, and year of movies Uma Thurman has acted in
+  # order them by ascending year
+  # hint: use 'select', 'joins', 'where', and 'order'
+
+
+  # Rails Console: Movie.select(:id, :title, :yr).joins(:actors).where('name = \'Uma Thurman\'').order('yr ASC')
+  Movie
+    .select(:id, :title, :yr)
+    .joins(:actors)
+    .where('name = \'Uma Thurman\'')
+    .order('yr ASC')
+end
